262ef3f0ea850b3b9968e8ee298adc498e4b4948
Hassan Abbas Rahupoto <hrahupoto@deakin.edu.au>
2020-10-09 19:33:57 +1100

    create html page with jquery and materialize

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ var moment = require('moment');
 
 var app = express();
 
-var log=function(message){
-var time=moment().format()
-console.log('[Server] @'+time+' '+message)
-}
+var log = function (message) {
+  var time = moment().format();
+  console.log('[Server] @' + time + ' ' + message);
+};
 
 app.use(express.static(__dirname + '/public'));
+
+var adder = function (num1, num2) {
+  var result = num1 + num2;
+  return result;
+};
+
+app.get('/adder', function (req, res) {
+  log('Adder Request Made');
+  var num1 = parseInt(req.query.num1);
+  var num2 = parseInt(req.query.num2);
+  var result = adder(num1, num2);
+  res.send('' + result + '');
+});
 
 var port = 3000;
 app.listen(port);
